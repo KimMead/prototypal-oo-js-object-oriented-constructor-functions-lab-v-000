8a7972c88454911057a3f69831ac9d42ec5e53f8
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2020-01-08 03:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function Scooter(year, color, model) {
     this.year = year;
     this.color = color;
-    this.model = model; {
-        console.log(`Hello everybody, my name is ${this.name} whom you've been
-mailing at ${this.email}!`);
-    };
+    this.model = model;
 }
-
-let lauren = new User('lauren', 'lauren@example.com');
-lauren.sayHello(); 
