40b8f20551a017dbb50b13f12239c348601bac94
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2020-01-08 03:18:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model; {
+        console.log(`Hello everybody, my name is ${this.name} whom you've been
+mailing at ${this.email}!`);
+    };
+}
+ 
+let lauren = new User('lauren', 'lauren@example.com');
+lauren.sayHello(); 
